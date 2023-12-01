b640c1de28538b3c988bd73c8fe53d38986ae832
mohamedsalah1224 <loding600@gmail.com>
2023-12-01 07:24:56 +0200

    Add Network Layer Helper with Dio Package

---
+++ lib/service/networking/dio_helper.dart
@@ -0,0 +1,42 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+
+import '../../utils/end_point.dart';
+import 'dio_Interceptors.dart';
+
+class DioHelper {
+  final _dio = createDio();
+  final tokenDio = Dio(BaseOptions(baseUrl: EndPoint.baseUrl));
+
+  DioHelper._internal();
+
+  static final _singleton = DioHelper._internal();
+
+  Dio get dio => _dio;
+
+  factory DioHelper() => _singleton;
+
+  static Dio createDio() {
+    var dio = Dio(BaseOptions(
+      baseUrl: EndPoint.baseUrl,
+      receiveTimeout: const Duration(seconds: 15), // 15 seconds
+      connectTimeout: const Duration(seconds: 15),
+      sendTimeout: const Duration(seconds: 15),
+    ));
+
+    dio.interceptors.addAll({
+      DioInterceptors(dio),
+    });
+
+    if (!kReleaseMode) {
+      dio.interceptors.add(LogInterceptor(
+          requestHeader: true,
+          requestBody: true,
+          responseHeader: true,
+          error: true,
+          responseBody: true,
+          request: true));
+    }
+    return dio;
+  }
+}
