491efb3cbc788f9ca894f68bd26e7335a8fa2017
mohamedsalah1224 <loding600@gmail.com>
2023-12-01 07:26:08 +0200

    Add People Repositry Service Implementation

---
+++ lib/service/api/repository_implementaion_service/pepole_repositry_service.dart
@@ -0,0 +1,76 @@
+import '../../../model/profile_person_image_model.dart';
+import '../../../utils/end_point.dart';
+import '../../networking/dio_helper.dart';
+import '../repository/pepole_repositry.dart';
+
+class PeopleRepositryService implements PepoleRepositry {
+  static PeopleRepositryService? _instance;
+
+  factory PeopleRepositryService() => _instance ??= PeopleRepositryService._();
+
+  PeopleRepositryService._();
+
+  @override
+  Future<ProfilePersonImageModel> getImages({required int personId}) async {
+    var response = await DioHelper().dio.get(
+        EndPoint.profilePersonImage(person_id: personId),
+        queryParameters: {
+          "api_key": "fdd781f0c3c5c144079ecc6c5ca41814",
+        });
+    return ProfilePersonImageModel.fromJson(response.data);
+  }
+
+  @override
+  getTaggedImages() {
+    // TODO: implement getTaggedImages
+    throw UnimplementedError();
+  }
+
+  @override
+  getChanges() {
+    // TODO: implement getChanges
+    throw UnimplementedError();
+  }
+
+  @override
+  getCombinedCredits() {
+    // TODO: implement getCombinedCredits
+    throw UnimplementedError();
+  }
+
+  @override
+  getDetails() {
+    // TODO: implement getDetails
+    throw UnimplementedError();
+  }
+
+  @override
+  getExternalIds() {
+    // TODO: implement getExternalIds
+    throw UnimplementedError();
+  }
+
+  @override
+  getLatest() {
+    // TODO: implement getLatest
+    throw UnimplementedError();
+  }
+
+  @override
+  getMovieCredits() {
+    // TODO: implement getMovieCredits
+    throw UnimplementedError();
+  }
+
+  @override
+  getTranslations() {
+    // TODO: implement getTranslations
+    throw UnimplementedError();
+  }
+
+  @override
+  getTvCredits() {
+    // TODO: implement getTvCredits
+    throw UnimplementedError();
+  }
+}
